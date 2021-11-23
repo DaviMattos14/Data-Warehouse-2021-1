6b8aa4e7e75b0f7b1e0704a2ff824135b5e063e3
DaviMattos14 <davimattos2012@gmail.com>
2021-11-23 17:55:55 -0300

    Banco de Dados

--- ENADE/3. Banco de Dados/CriandoBancoDados.py
+++ ENADE/3. Banco de Dados/CriandoBancoDados.py
@@ -4,7 +4,7 @@ bd_connection = sqlite3.connect("ENADE.db")
 
 cursor = bd_connection.cursor()
 
-sql_script = open("/Users/conta/Desktop/Data-Warehouse-2021-1-1/ENADE/3. Banco de Dados/SCRIPT_BD_ENADE.sql")
+sql_script = open("/Users/conta/Desktop/Data-Warehouse-2021-1/ENADE/3. Banco de Dados/SCRIPT_BD_ENADE.sql")
 
 sql_str = sql_script.read()
 
