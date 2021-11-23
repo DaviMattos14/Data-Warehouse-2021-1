df4f131f5197bac90039d52fae8f0d2222db8340
DaviMattos14 <davimattos2012@gmail.com>
2021-11-23 16:37:03 -0300

    Banco de Dados

---
+++ ENADE/3. Banco de Dados/teste.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+dados = pd.read_csv("C:/Users/conta/Desktop/Data-Warehouse-2021-1/ENADE/1. Coleta de Dados/DadosENADE/3.DADOS/microdados_enade_2019.txt", 
+sep = ";", 
+decimal=",")
