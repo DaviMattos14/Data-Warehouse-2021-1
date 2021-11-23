fd11c526f114591b3c103dba714e2bb1f5f74157
DaviMattos14 <davimattos2012@gmail.com>
2021-11-23 17:51:27 -0300

    Modelagem Dimensional

--- ENADE/3. Banco de Dados/teste.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-dados = pd.read_csv("C:/Users/conta/Desktop/Data-Warehouse-2021-1/ENADE/1. Coleta de Dados/DadosENADE/3.DADOS/microdados_enade_2019.txt", 
-sep = ";", 
-decimal=",")
