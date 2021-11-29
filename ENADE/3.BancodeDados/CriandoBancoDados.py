477307a7cb16947fe3ed3d0774eba60e84cc505c
LulianoM <martinslucianofigueira@gmail.com>
2021-11-28 22:25:07 -0300

    adicionando automatização com o make

--- ENADE/3.BancodeDados/CriandoBancoDados.py
+++ ENADE/3.BancodeDados/CriandoBancoDados.py
@@ -4,7 +4,9 @@ bd_connection = sqlite3.connect("ENADE.db")
 
 cursor = bd_connection.cursor()
 
-sql_script = open("/Users/conta/Desktop/Data-Warehouse-2021-1/ENADE/3. Banco de Dados/SCRIPT_BD_ENADE.sql")
+sql_script = open(
+    r"/Users/lulianom/Documents/Codes/Data-Warehouse-2021-1/ENADE/3.BancodeDados/SCRIPT_BD_ENADE.sql"
+)
 
 sql_str = sql_script.read()
 
